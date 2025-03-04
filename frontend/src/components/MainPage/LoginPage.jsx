32330b85c4c38c3b5604e468a362db6b85261a51
dakshataramteke <dakshataramteke@skyvisionitsolutions.in>
2025-03-04 18:15:21 +0530

    Registration Validations

--- frontend/src/components/MainPage/LoginPage.jsx
+++ frontend/src/components/MainPage/LoginPage.jsx
@@ -1,3 +1,174 @@
+// import { React, useState } from "react";
+// import { NavLink, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import Swal from "sweetalert2";
+// import Cookies from "js-cookie";
+// import CryptoJS from "crypto-js";
+// import "./LoginPage.css";
+// import VisibilityIcon from '@mui/icons-material/Visibility'
+// const LoginPage = () => {
+//   const SECRET_KEY = "abhishek";
+
+//   const [values, setValues] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const [isAgreed, setIsAgreed] = useState(false); // New state for checkbox
+
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setValues((prevValues) => ({
+//       ...prevValues,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleCheckboxChange = () => {
+//     setIsAgreed((prev) => !prev); // Toggle checkbox state
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+    
+//     if (!isAgreed) { // Check if the checkbox is checked
+//       Swal.fire({
+//         title: "Error!",
+//         text: "You must agree to the terms and conditions to log in.",
+//         icon: "error",
+//       });
+//       return; // Stop the submission process
+//     }
+
+//     console.log(values);
+
+//     try {
+//       const res = await axios.post("http://localhost:8080/login", values);
+//       console.log("response login is : ", res.data.name);
+
+//       if (res.status === 200) {
+//         console.log("response login is : ", res.data.name);
+//         localStorage.setItem("Login User", res.data.name);
+//         localStorage.setItem("userEmail", values.email);
+
+//         Swal.fire({
+//           title: "Successfully ",
+//           text: "Logged in successfully!",
+//           icon: "success",
+//         });
+//         const encrypted = CryptoJS.AES.encrypt(
+//           JSON.stringify(values.email),
+//           SECRET_KEY
+//         ).toString();
+//         Cookies.set("user", encrypted, { path: "/" });
+//         console.log("encrypt data", encrypted);
+//         navigate("/home");
+//       }
+//       setValues({
+//         email: "",
+//         password: "",
+//       });
+//     } catch (err) {
+//       if (err.response && err.response.status === 401) {
+//         Swal.fire({
+//           title: "Error!",
+//           text: "Check UserName and Password",
+//           icon: "error",
+//         });
+//       } else {
+//         Swal.fire({
+//           title: "Error!",
+//           text: "Please fill out your username and password",
+//           icon: "error",
+//         });
+//       }
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <section className="home_wrapper" style={{ backgroundColor: "#c4ccd" }}>
+//         <div className="container-fluid">
+//           <div className="row home_formPage">
+//             <div className="col-12 col-md-6 login_Page"></div>
+//             <div className="col-12 col-md-6 p-md-5 p-4 home_form">
+//             <div className="pg">
+//             <h1 className="text-center mb-4 ">Login </h1>
+//               <form onSubmit={handleSubmit}>
+
+//                 <div className="mb-3">
+//                   <label htmlFor="email" className="form-label">
+//                     <b> Email address</b> 
+//                   </label>
+//                   <input
+//                     type="email"
+//                     className="form-control"
+//                     id="email"
+//                     name="email"
+//                     placeholder="Enter your email address"
+//                     aria-describedby="emailHelp"
+//                     autoComplete="off"
+//                     value={values.email}
+//                     onChange={handleChange}
+//                   />
+//                 </div>
+
+//                 <div className="mb-3">
+//                   <label htmlFor="password" className="form-label ">
+//                     <b>Password </b>
+//                   </label>
+//                   <input
+//                   {
+//                     type="password"? "text"
+//                   }
+//                    togglevisibility :true;
+//                     className="form-control"
+//                     id="password"
+//                     name="password"
+//                     placeholder="Enter your password"
+//                     value={values.password}
+//                     onChange={handleChange}
+//                     autoComplete="off"
+//                     aria-describedby="password"
+//                   />
+//                 </div>
+//                 <div className="mb-3 form-check">
+//                   <input
+//                     type="checkbox"
+//                     className="form-check-input"
+//                     id="Check1"
+//                     checked={isAgreed}
+//                     onChange={handleCheckboxChange} // Update checkbox state
+//                   />
+//                   <label className="form-check-label text-muted" htmlFor="Check1" style={{fontSize:'0.875rem'}}>
+//                     By Signing up I Agree with <span className="text-primary">Terms & Condition</span> 
+//                   </label>
+//                 </div>
+//                 <div className="my-4 d-flex justify-content-center">
+//                 <button type="submit" className="me-2 btn btn-outline-secondary">
+//                     Sign In
+//                   </button>
+//                   <NavLink to="/signup" className="btn btn-primary  ms-md-3 ms-lg-5 ms-2">
+//                     Sign Up
+//                   </NavLink>
+                 
+//                 </div>
+//               </form>
+//             </div>
+            
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default LoginPage;
+
 import { React, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -5,6 +176,8 @@ import Swal from "sweetalert2";
 import Cookies from "js-cookie";
 import CryptoJS from "crypto-js";
 import "./LoginPage.css";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const LoginPage = () => {
   const SECRET_KEY = "abhishek";
@@ -15,6 +188,7 @@ const LoginPage = () => {
   });
 
   const [isAgreed, setIsAgreed] = useState(false); // New state for checkbox
+  const [showPassword, setShowPassword] = useState(false); // New state for password visibility
 
   const navigate = useNavigate();
 
@@ -28,6 +202,10 @@ const LoginPage = () => {
 
   const handleCheckboxChange = () => {
     setIsAgreed((prev) => !prev); // Toggle checkbox state
+  };
+
+  const handlePasswordVisibilityToggle = () => {
+    setShowPassword((prev) => !prev); // Toggle password visibility
   };
 
   const handleSubmit = async (e) => {
@@ -95,67 +273,71 @@ const LoginPage = () => {
           <div className="row home_formPage">
             <div className="col-12 col-md-6 login_Page"></div>
             <div className="col-12 col-md-6 p-md-5 p-4 home_form">
-            <div className="pg">
-            <h3 className="text-center mb-4 ">Login </h3>
-              <form onSubmit={handleSubmit}>
+              <div className="pg">
+                <h1 className="text-center mb-4 ">Login </h1>
+                <form onSubmit={handleSubmit}>
 
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    <b> Email address</b> 
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    name="email"
-                    placeholder="name@gmail.com"
-                    aria-describedby="emailHelp"
-                    autoComplete="off"
-                    value={values.email}
-                    onChange={handleChange}
-                  />
-                </div>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      <b> Email address</b> 
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="Enter your email address"
+                      aria-describedby="emailHelp"
+                      autoComplete="off"
+                      value={values.email}
+                      onChange={handleChange}
+                    />
+                  </div>
 
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label ">
-                    <b>Password </b>
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={values.password}
-                    onChange={handleChange}
-                    autoComplete="off"
-                    aria-describedby="password"
-                  />
-                </div>
-                <div className="mb-3 form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="Check1"
-                    checked={isAgreed}
-                    onChange={handleCheckboxChange} // Update checkbox state
-                  />
-                  <label className="form-check-label text-muted" htmlFor="Check1" style={{fontSize:'0.875rem'}}>
-                    By Signing up I Agree with <span className="text-primary">Terms & Condition</span> 
-                  </label>
-                </div>
-                <div className="my-4 d-flex justify-content-center">
-                <button type="submit" className="me-2 btn btn-outline-secondary">
-                    Sign In
-                  </button>
-                  <NavLink to="/signup" className="btn btn-primary  ms-md-3 ms-lg-5 ms-2">
-                    Sign Up
-                  </NavLink>
-                 
-                </div>
-              </form>
-            </div>
-            
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label ">
+                      <b>Password </b>
+                    </label>
+                    <div className="input-group">
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={values.password}
+                        onChange={handleChange}
+                        autoComplete="off"
+                        aria-describedby="password"
+                        style={{ letterSpacing: "1px",  padding: '10px', marginBottom: "5px" }}
+                      />
+                      <span className="input-group-text" onClick={handlePasswordVisibilityToggle} style={{ cursor: 'pointer' }}>
+                        {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="Check1"
+                      checked={isAgreed}
+                      onChange={handleCheckboxChange} // Update checkbox state
+                    />
+                    <label className="form-check-label text-muted" htmlFor="Check1" style={{fontSize:'0.875rem'}}>
+                      By Signing up I Agree with <span className="text-primary">Terms & Conditions</span> 
+                    </label>
+                  </div>
+                  <div className="my-4 d-flex justify-content-center">
+                    <button type="submit" className="me-2 btn btn-outline-secondary">
+                      Sign In
+                    </button>
+                    <NavLink to="/signup" className="btn btn-primary  ms-md-3 ms-lg-5 ms-2">
+                      Sign Up
+                    </NavLink>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/components/MainPage/SignUpPage.jsx
+++ frontend/src/components/MainPage/SignUpPage.jsx
@@ -1,7 +1,228 @@
+// import React, { useState } from "react";
+// import { Link, Navigate } from "react-router-dom";
+// import axios from "axios";
+// import Swal from "sweetalert2";
+// const SignUpPage = () => {
+//   const [values, setValues] = useState({
+//     name: "",
+//     email: "",
+//     mobile: "",
+//     password: "",
+//     username: "",
+//     city: "",
+//   });
+
+//   // const [loading, setLoading] = useState(false);
+//   const [redirect, setRedirect] = useState(false); // State to control redirection
+
+//   const handleChange = (e) => {
+//     setValues({ ...values, [e.target.name]: e.target.value });
+//   };
+
+//   const submitData = (e) => {
+//     e.preventDefault();
+//     // setLoading(true);
+//     console.log(values);
+//     const form = e.target;
+//     if (!form.checkValidity()) {
+//       // If the form is invalid, trigger the invalid feedback
+//       form.classList.add('was-validated');
+//       return; // Prevent submission
+//     }
+//     axios
+//       .post("http://localhost:8080/signup", values)
+//       .then((res) => {
+//         // setLoading(false);
+//         Swal.fire({
+//           title: "Successful!",
+//           text: "Your Form is Submitted!",
+//           icon: "success",
+//         });
+//         setRedirect(true); // Set redirect to true
+//         setValues({
+//           name: "",
+//           email: "",
+//           mobile: "",
+//           password: "",
+//           username: "",
+//           city: "",
+//         });
+//       })
+//       .catch((err) => {
+//         // setLoading(false);
+//         console.log(err);
+//         const errorMessage =
+//           err.response?.data || "An error occurred. Please try again.";
+//         Swal.fire({
+//           title: "Error!",
+//           text: errorMessage,
+//           icon: "error",
+//         });
+//       });
+//   };
+
+//   // Redirect to login page if redirect is true
+//   if (redirect) {
+//     return <Navigate to="/" />;
+//   }
+
+//   return (
+//     <>
+//       <section className="signup_wrapper">
+//         <div className="container-fluid">
+//           <div className="row signup_formPage">
+//             <div className="col-12 col-md-6 signup_sidebar"></div>
+//             <div className="col-12 col-md-6 col-lg-6 py-md-4 signup_righsidebar">
+//               <h2 className="text-center">Registration</h2>
+//               <p className="text-center mb-4">
+//                 Take a minute and fill the Details to Register
+//               </p>
+//               <form
+//                 onSubmit={submitData}
+//                 className="needs-validation"
+//                 noValidate
+//               >
+//                 <div className="my-md-3">
+//                   <label htmlFor="Full_Name" className="form-label">
+//                     Name <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="Full_Name"
+//                     name="name"
+//                     placeholder="Enter your name"
+//                     aria-describedby="full_name"
+//                     value={values.name}
+//                     onChange={handleChange}
+//                     required
+//                     pattern="^[a-zA-Z ]*$"
+//                   />
+//                   <div class="invalid-feedback">Please enter a name.</div>
+//                 </div>
+//                 <div className="mb-3">
+//                   <label htmlFor="email" className="form-label">
+//                     Email address <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="email"
+//                     className="form-control"
+//                     id="email"
+//                     name="email"
+//                     placeholder="example@gmail.com"
+//                     aria-describedby="email"
+//                     value={values.email}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                   <div class="invalid-feedback">Please enter a email.</div>
+//                 </div>
+
+//                 <div className="mb-3">
+//                   <label htmlFor="mobile" className="form-label">
+//                     Mobile <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="mobile"
+//                     name="mobile"
+//                     placeholder="Enter your mobile number"
+//                     value={values.mobile}
+//                     onChange={handleChange}
+//                     required
+//                     aria-describedby="emailHelp"
+//                     pattern="[0-9]{10}"
+//                   />
+//                   <div class="invalid-feedback">Please enter a mobile number.</div>
+//                 </div>
+//                 <div className="mb-3">
+//                   <label htmlFor="password" className="form-label">
+//                     App Password <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="password"
+//                     className="form-control"
+//                     id="password"
+//                     name="password"
+//                     placeholder="********"
+//                     aria-describedby="password"
+//                     value={values.password}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                   <div class="invalid-feedback">Please enter a password.</div>
+//                 </div>
+//                 <div className="mb-3">
+//                   <label htmlFor="username" className="form-label">
+//                     Username <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="username"
+//                     name="username"
+//                     placeholder="Username"
+//                     aria-describedby="username"
+//                     value={values.username}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                   <div class="invalid-feedback">Please enter a username.</div>
+//                 </div>
+//                 <div className="mb-3">
+//                   <label htmlFor="city" className="form-label">
+//                     City <span className="text-danger">*</span>
+//                   </label>
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="city"
+//                     name="city"
+//                     placeholder="Enter your city"
+//                     aria-describedby="city"
+//                     value={values.city}
+//                     onChange={handleChange}
+//                     required
+//                   />
+//                   <div class="invalid-feedback">Please enter a city.</div>
+//                 </div>
+
+//                 <div className="d-flex justify-content-center">
+//                   <button
+//                     type="submit"
+//                     className="btn btn-primary"
+//                     // disabled={loading}
+//                   >
+//                   Register
+//                     {/* {loading ? "Loading...." : "Register"} */}
+//                   </button>
+//                 </div>
+//                 <div className="mt-3 text-center">
+//                   <p>
+//                     Already have an account?{" "}
+//                     <Link to="/" className="fw-bold">
+//                       Log In
+//                     </Link>
+//                   </p>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default SignUpPage;
+
+
 import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
+
 const SignUpPage = () => {
   const [values, setValues] = useState({
     name: "",
@@ -12,17 +233,21 @@ const SignUpPage = () => {
     city: "",
   });
 
-  const [loading, setLoading] = useState(false);
   const [redirect, setRedirect] = useState(false); // State to control redirection
-
   const handleChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+  
+    // If the input is empty or only contains whitespace, allow leading whitespace
+    if (value.trim() === "") {
+      setValues({ ...values, [name]: value });
+    } else {
+      // Otherwise, trim leading whitespace after the first character
+      setValues({ ...values, [name]: value.replace(/^\s+/, '') });
+    }
   };
 
   const submitData = (e) => {
     e.preventDefault();
-    setLoading(true);
-    console.log(values);
     const form = e.target;
     if (!form.checkValidity()) {
       // If the form is invalid, trigger the invalid feedback
@@ -32,10 +257,9 @@ const SignUpPage = () => {
     axios
       .post("http://localhost:8080/signup", values)
       .then((res) => {
-        setLoading(false);
         Swal.fire({
           title: "Successful!",
-          text: "Your Form is Submitted!",
+          text: "Registration Successfully!",
           icon: "success",
         });
         setRedirect(true); // Set redirect to true
@@ -49,7 +273,6 @@ const SignUpPage = () => {
         });
       })
       .catch((err) => {
-        setLoading(false);
         console.log(err);
         const errorMessage =
           err.response?.data || "An error occurred. Please try again.";
@@ -96,9 +319,9 @@ const SignUpPage = () => {
                     value={values.name}
                     onChange={handleChange}
                     required
-                    pattern="^[a-zA-Z]*$"
+                    pattern="^[a-zA-Z ]*$" 
                   />
-                  <div class="invalid-feedback">Please enter a name.</div>
+                  <div className="invalid-feedback">Please enter a valid name.</div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="email" className="form-label">
@@ -114,8 +337,9 @@ const SignUpPage = () => {
                     value={values.email}
                     onChange={handleChange}
                     required
+                    pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
                   />
-                  <div class="invalid-feedback">Please enter a email.</div>
+                  <div className="invalid-feedback">Please enter an email.</div>
                 </div>
 
                 <div className="mb-3">
@@ -131,10 +355,9 @@ const SignUpPage = () => {
                     value={values.mobile}
                     onChange={handleChange}
                     required
-                    aria-describedby="emailHelp"
                     pattern="[0-9]{10}"
                   />
-                  <div class="invalid-feedback">Please enter a mobile number.</div>
+                  <div className="invalid-feedback">Please enter a valid mobile number.</div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="password" className="form-label">
@@ -150,8 +373,9 @@ const SignUpPage = () => {
                     value={values.password}
                     onChange={handleChange}
                     required
+                    pattern="[a-zA-Z0-9]{6-15}"
                   />
-                  <div class="invalid-feedback">Please enter a password.</div>
+                  <div className="invalid-feedback">Please enter a password.</div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="username" className="form-label">
@@ -167,8 +391,9 @@ const SignUpPage = () => {
                     value={values.username}
                     onChange={handleChange}
                     required
+                    pattern="^[a-zA-Z ]*$" 
                   />
-                  <div class="invalid-feedback">Please enter a username.</div>
+                  <div className="invalid-feedback">Please enter a username.</div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="city" className="form-label">
@@ -184,17 +409,17 @@ const SignUpPage = () => {
                     value={values.city}
                     onChange={handleChange}
                     required
+                    pattern="^[a-zA-Z]*$" 
                   />
-                  <div class="invalid-feedback">Please enter a city.</div>
+                  <div className="invalid-feedback">Please enter a city.</div>
                 </div>
 
                 <div className="d-flex justify-content-center">
                   <button
                     type="submit"
                     className="btn btn-primary"
-                    disabled={loading}
                   >
-                    {loading ? "Loading...." : "Register"}
+                    Register
                   </button>
                 </div>
                 <div className="mt-3 text-center">
